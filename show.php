910ba2d488194f2fbc9e2188d7403cd300cf55d5
Jit <73680813+Jitzkung@users.noreply.github.com>
2020-11-17 16:04:37 +0700

    Update show.php

--- show.php
+++ show.php
@@ -5,7 +5,7 @@
 <body>
 <?php
 $conn = mysqli_init();
-mysqli_real_connect($conn, 'jitmysql.mysql.database.azure.com', 'jitmysql@jitmysql', '0894060563zxJITsy', 'itflab', 3306);
+mysqli_real_connect($conn, 'jitmysql.mysql.database.azure.com', 'jitmysql@jitmysql', 'JITsql123', 'itflab', 3306);
 if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
